fcf50296f6b56474de1a1396f7ef135442cfbac8
Maiknoebauer <maik.noebauer@students.hs-mainz.de>
2021-11-16 08:36:06 +0100

    New Scene Hausaufgabe05 and variable math script

--- Assets/Scripts/DebugLog.cs
+++ Assets/Scripts/DebugLog.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 public class DebugLog : MonoBehaviour
 {
     // Start is called before the first frame update
-    private new int number;
-    private new int count;
+    private new int _number;
+    private new int _count;
     void Start()
     {
         Debug.Log("Here, read my debug text");
-        number = 1;
-        Debug.Log(number);
+        _number = 1;
+        Debug.Log(_number);
         Debug.Log("Text:" + gameObject.name);
         Debug.Log("<color=red>Error: </color> Not found!");
-        count = 0;
+        _count = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        count++;
-        if (count<= 100)
+        _count++;
+        if (_count<= 100)
         {
-            Debug.Log(count);
+            Debug.Log(_count);
         }
     }
 }
