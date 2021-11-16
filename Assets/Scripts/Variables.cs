101b2c6f5ecc8fc6b276da8f91febba4c7b2822d
Maiknoebauer <maik.noebauer@students.hs-mainz.de>
2021-11-16 08:42:33 +0100

    string added and Development 1/3 finished

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -12,10 +12,12 @@ public class Variables : MonoBehaviour
     public int numberFour; 
     public int numberFive;
     private float variableDivision;
+    public string mySentence;
     void Start()
     {
         variableDivision = (numberOne + numberTwo + numberThree + numberFour + numberFive) / 3;
         Debug.Log(variableDivision);
+        Debug.Log(mySentence + " " + variableDivision);
     }
 
     // Update is called once per frame
